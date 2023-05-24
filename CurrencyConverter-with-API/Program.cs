4fef40d270d18868938d0373d425ee01a4021497
Joanna Michalska <asiamichalska9@gmail.com>
2023-05-24 23:28:14 +0200

    Main logic, installed packages, added API connection

--- CurrencyConverter-with-API/Program.cs
+++ CurrencyConverter-with-API/Program.cs
@@ -1,10 +1,74 @@
-﻿namespace CurrencyConverter_with_API
+﻿using Newtonsoft.Json;
+using System.Net.Http.Json;
+
+namespace CurrencyConverter_with_API
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter the amount to convert: ");
+            var amount = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the currency to convert from: ");
+            var fromCurrency = Console.ReadLine().ToUpper();
+
+            Console.Write("Enter the currency to convert to: ");
+            var toCurrency = Console.ReadLine().ToUpper();
+
+            try
+            {
+                var exchangeRate = await GetExchangeRate(fromCurrency, toCurrency);
+                var convertedAmount = ConvertCurrency(amount, exchangeRate);
+
+                Console.WriteLine($"{amount} {fromCurrency} = {convertedAmount} {toCurrency}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
         }
+
+        static async Task<double> GetExchangeRate(string fromCurrency, string toCurrency)
+        {
+            var apiKey = "---PLACEHOLDER---";
+            var apiUrl = $"https://api.currencyapi.com/v3/latest?apikey={apiKey}&base_currency={fromCurrency}&currencies={toCurrency}";
+
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.GetAsync(apiUrl);
+                var json = await response.Content.ReadAsStringAsync();
+                var currencyResponse = JsonConvert.DeserializeObject<CurrencyResponse>(json);
+
+                if (currencyResponse == null)
+                {
+                    throw new Exception("Failed to fetch exchange rate");
+                }
+
+                return currencyResponse.Data.ToList().First().Value.Value;
+            }
+        }
+
+        static double ConvertCurrency(double amount, double exchangeRate)
+        {
+            return amount * exchangeRate;
+        }
+    }
+
+    public class CurrencyData
+    {
+        public string Code { get; set; }
+        public double Value { get; set; }
+    }
+
+    public class CurrencyResponse
+    {
+        public MetaData Meta { get; set; }
+        public Dictionary<string, CurrencyData> Data { get; set; }
+    }
+
+    public class MetaData
+    {
+        public DateTime LastUpdatedAt { get; set; }
     }
 }
